64d622dbd0798dd4281b4fdb9de8d00035ba2242
gabriel-severo <gabriel.ivi@hotmail.com>
2020-04-04 17:14:50 -0300

    Made some optimization

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ async function getStreamers(){
 	let streams = ''
 	const newWindow = window.open('https://mixer.com/browse/games/1386')
 	await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            streams = newWindow.document.getElementsByClassName('container_ZqIuE cardStyle_8PoHy')
+		newWindow.onload = () => {
+			streams = newWindow.document.getElementsByClassName('container_ZqIuE cardStyle_8PoHy')
 			newWindow.close()
 			resolve()
-        }, 3000);
-    });
+		}		
+	})
 	return streams
 }
 
@@ -19,12 +19,15 @@ async function openStream(){
 
 async function setOnlyAudio(){
 	await openStream()
-	await new Promise((resolve, reject) => {
-		setTimeout(() => {
-			janela.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')[1].click()
-			resolve()
-		}, 20000)
-	})
+	janela.onload = () => {
+		while (true){
+			resultado = janela.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')
+			if (resultado != null){
+				break
+			}
+		}
+		janela.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')[1].click()
+	}
 }
 
 setOnlyAudio()
