75640bd62e21c092bc33b17e1d7c44b166c945b9
gabriel-severo <gabriel.ivi@hotmail.com>
2020-04-05 20:52:29 -0300

    Adding main and hasLoot function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let janela = ''
 function getStreamers(newWindow){
 	streams = newWindow.document.getElementsByClassName('container_ZqIuE cardStyle_8PoHy')
 	return streams
 }
 
-async function openStream(){
+async function openStream(num){
+	let streamWindow
 	const newWindow = window.open('https://mixer.com/browse/games/1386')
 	await new Promise((resolve, reject) => {
 		newWindow.onload = () => {
@@ -15,7 +15,7 @@ async function openStream(){
 	await new Promise((resolve, reject) => {
 		let intervalo = setInterval(() => {
 			try{
-				janela = window.open(streams[0].href)
+				streamWindow = window.open(streams[num].href)
 				newWindow.close()
 				clearInterval(intervalo)
 			}catch(e){
@@ -24,21 +24,49 @@ async function openStream(){
 			resolve()
 		}, 1000)			
 	})
+	return streamWindow
 }
 
-async function setOnlyAudio(){
-	await openStream()
-	janela.onload = () => {
-		let intervalo = setInterval(() => {
-			resultado = janela.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')
-			try{
-				janela.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')[1].click()
-				clearInterval(intervalo)
-			}catch(e){
-				console.log(e)
-			}
-		}, 1000)
+async function setOnlyAudio(streamWindow){
+	await new Promise((resolve, reject) => {		
+		streamWindow.onload = () => {
+			let intervalo = setInterval(() => {
+				resultado = streamWindow.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')
+				try{
+					streamWindow.document.getElementsByClassName('_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm')[1].click()
+					clearInterval(intervalo)
+					resolve()
+				}catch(e){
+					console.log(e)
+				}
+			}, 1000)
+		}
+	})
+}
+
+function hasLoot(stream){
+	let cont = stream.document.getElementsByClassName('subTitle_2Lhiz').length
+	if (cont == 0){
+		return false
+	}else{
+		return true
 	}
 }
 
-setOnlyAudio()
+
+async function main(){
+	let num = 0
+	while(true){
+		let streamWindow = await openStream(num)
+		await setOnlyAudio(streamWindow)
+		let loot = hasLoot(streamWindow)
+		if(loot){
+			break
+		}else{
+			streamWindow.close()
+		}
+		num++
+	}
+}
+
+main()
